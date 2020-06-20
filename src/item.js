35bf2c4d51dfc70658f579ca5e482fd5f19e7c67
Eddy Recio <eddy.recio@gmail.com>
2020-06-19 20:02:02 -0700

    Handle for moving items - Created new handle component - Integrated into items

--- src/item.js
+++ src/item.js
@@ -8,26 +8,29 @@ const Container = styled.div`
   padding: 8px;
   margin-bottom: 8px;
   background-color: ${props => (props.isDragging ? "lightgreen" : "white")};
+  display: flex;
 `;
 
-// const Handle = styled.div`
-//   width: 20px;
-//   height: 20px;
-//   background-color: orange;
-//   border-radius: 4px;
-// `;
+const Handle = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: orange;
+  border-radius: 4px;
+  margin-right: 8px;
+`;
 
 const Item = props => {
   return (
     <Draggable draggableId={props.item.id} index={props.index}>
       {(provided, snapshot) => (
+
         <Container
           {...provided.draggableProps}
-          {...provided.dragHandleProps}
+          // {...provided.dragHandleProps}
           ref={provided.innerRef}
           isDragging={snapshot.isDragging}
         >
-          {/* <Handle {...provided.dragHandleProps} /> */}
+          <Handle {...provided.dragHandleProps} />
           {props.item.content}
         </Container>
       )}
